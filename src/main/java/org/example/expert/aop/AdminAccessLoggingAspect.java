fdbe6cf640e3a8e3db4147c02c8280206c143f00
고나은 <gonaeunn@gmail.com>
2025-05-09 22:50:15 +0900

    Level1-5 AOP의 포인트컷 변경

--- src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
+++ src/main/java/org/example/expert/aop/AdminAccessLoggingAspect.java
@@ -18,7 +18,7 @@ public class AdminAccessLoggingAspect {
 
     private final HttpServletRequest request;
 
-    @After("execution(* org.example.expert.domain.user.controller.UserController.getUser(..))")
+    @After("execution(* org.example.expert.domain.user.controller.UserAdminController.changeUserRole(..))") // aop 적용될 포인트컷 변경
     public void logAfterChangeUserRole(JoinPoint joinPoint) {
         String userId = String.valueOf(request.getAttribute("userId"));
         String requestUrl = request.getRequestURI();
